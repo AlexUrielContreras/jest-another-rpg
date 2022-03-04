da7fedeaa05e978119b43767481d237d30eb29a9
Alex Contreras <alex.code29@gmail.com>
2022-03-04 14:13:15 -0600

    added isAlive and reduceHealth methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -16,3 +16,50 @@ test('create a player object' , () => {
     );
 });
 
+test('gets player stats as an object', () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+    
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+});
+
+test("gets player's health value", () => {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()))
+})
+
+test('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+    
+    expect(player.isAlive()).toBeFalsy();
+})
+
+test('subtract from player health', () => {
+    const player = new Player('Dave')
+    const oldHealth = player.health
+    
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999)
+
+    expect(player.health).toBe(0)
+})
